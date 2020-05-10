61ff720c8e64def7b39c5d20580b9778929d74e8
b.nath.shukla <b.nath.shukla@WIN-IKTQNR930W.corp.avanade.org>
2020-05-09 19:48:53 -0500

    Jenkins projectfirst

---
+++ Jenkins_Practice/src/test/java/JenkinsPractice/Jenkins_Practice/JekinsGitHub.java
@@ -0,0 +1,15 @@
+package JenkinsPractice.Jenkins_Practice;
+
+import org.testng.annotations.Test;
+
+public class JekinsGitHub {
+	
+	@Test
+	public void Sum()
+	{
+		System.out.println("This is Jenkins Practice");
+	}
+	
+	
+
+}
